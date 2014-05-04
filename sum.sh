304b423e7e19e6b459892b336b6b09573fb498cf
oishi <oishi@feedtailor.jp>
2014-05-04 15:04:43 +0900

    ALLでcurrencyに関係無く合計値を計算するように Unit合計だけでなくProceedの合計も表示

--- sum.sh
+++ sum.sh
@@ -13,9 +13,16 @@ else
 fi
 
 # totaling.shを呼び出して集計用CTVを生成
-./totaling.sh ${@:3} | grep $1 | grep $2 | awk 'BEGIN {FS="\t"} {printf("%s\t%s\n"),$3,$4}' > ${TEMP}
+if [ ${2} = "ALL" ]; then
+	./extract.sh ${@:3} | grep $1 | awk 'BEGIN {FS="\t"} {printf("%s\t%s\n"),$3,$4}' > ${TEMP}
+else
+	./extract.sh ${@:3} | grep $1 | grep $2 | awk 'BEGIN {FS="\t"} {printf("%s\t%s\n"),$3,$4}' > ${TEMP}
+fi
 
 # perlのワンライナーで集計
-perl -ne 'BEGIN{$t=0;}/^[0-9]+?/ and $t += $_;END{printf("$t\n");}' ${TEMP}
+perl -ne 'BEGIN{$t=0;$p=0;}/^([0-9]+?)\t([0-9\.]*)/ and $t += $1; $p += $2;END{printf("Unit=$t, Proceed=$p\n");}' ${TEMP}
 
-
+# 後始末
+if [ -e ${TEMP} ]; then
+	rm -f ${TEMP}
+fi
